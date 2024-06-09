8dfc241aa32040b3ab30e3189164dc30ba31301b
sa8can <sirotama1465@gmail.com>
2024-06-10 00:21:23 +0900

    Change output directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import fs from 'fs';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/app.js`,
+      },
+    },
+  },
   server: {
     port: 3000,
     watch: {

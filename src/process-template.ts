54ddc467869b8fc84f9c48f51ff3b430927ebc50
sa8can <sirotama1465@gmail.com>
2024-06-11 20:44:22 +0900

    Update process-template.ts

--- src/process-template.ts
+++ src/process-template.ts
@@ -1,17 +1,24 @@
-import { Mat } from 'opencv-ts';
+import cv, { Mat } from 'opencv-ts';
 import OpenCVWebCamHelper from './helper/opencv-with-webcam';
 
 export default class OpenCVProcess {
+  private manager: OpenCVWebCamHelper;
+
   constructor() {
-    new OpenCVWebCamHelper(
+    this.manager = new OpenCVWebCamHelper(
       () => this.init(),
       (Mat) => this.process(Mat),
     );
   }
 
+  // Call on opencv-ts runtime initialized
   public init() {}
 
+  // Call every frame
   public process(currFrame: Mat): Mat {
-    return currFrame;
+    const gray = new cv.Mat();
+    cv.cvtColor(currFrame, gray, cv.COLOR_BGR2GRAY);
+    
+    return gray;
   }
 }
